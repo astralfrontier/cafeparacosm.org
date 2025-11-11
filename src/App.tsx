8c15ea1624da8eccea6878337f7127208f395e5b
Bill Garrett <garrett@astralfrontier.org>
2025-11-11 14:09:48 -0800

    Initial import

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="container">
+        <h1>Cafe Paracosm</h1>
+      </header>
+      <main className="container">
+        <p>Coming soon</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
