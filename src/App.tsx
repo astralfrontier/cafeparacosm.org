269da8ac9d8f3a2ca434790ec2fd8e8e4494f1b0
Bill Garrett <garrett@astralfrontier.org>
2025-11-12 09:51:02 -0800

    Add redirect link

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { useLocation } from "react-router";
 import "./App.css";
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
       <header className="container">
@@ -8,6 +11,12 @@ function App() {
       </header>
       <main className="container">
         <p>Coming soon</p>
+        <p>
+          Were you looking for a post on the forum? Try{" "}
+          <a
+            href={`https://cafeparacosm.forum${location.pathname}`}
+          >{`https://cafeparacosm.forum${location.pathname}`}</a>
+        </p>
       </main>
     </>
   );
