7f6e692c21ee25e6984e84eb89339ea5f8c59623
Bill Garrett <garrett@astralfrontier.org>
2026-01-06 17:29:02 -0800

    Remove Vite files

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import netlify from "@netlify/vite-plugin";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [netlify(), react()],
-});
